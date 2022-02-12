22b9d7f7b095ee5cd435da9d1d4c6a2502ecd158
jesvica <jvictoriaw@hotmail.co.uk>
2022-02-12 23:24:18 +0000

    task 1

--- session01/index.js
+++ session01/index.js
@@ -1,1 +1,22 @@
-// Add the code here
+  // Add the code here
+'use strict';
+
+function ClickButton() {
+
+  const [clicked, setClick] = React.useState(false);
+
+  if (clicked) {
+    return 'Nothing here yet! Change this text to add more info.';
+  }
+
+  return React.createElement(
+    'button', {
+    className: "main-button",
+    onClick: () => setClick(true)
+  },
+    'Click To Reveal'
+  );
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(React.createElement(ClickButton), domContainer);
