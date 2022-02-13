78f65d8c547835509385c541701559743570250e
jesvica <jvictoriaw@hotmail.co.uk>
2022-02-13 16:30:37 +0000

    task 1

--- session02/src/App.js
+++ session02/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+const h1Style = {
+  "fontSize": "30px",
+  "color": "#cc0000",
+  "textTransform": "uppercase"
+};
+
   return (
     <div className="App">
-      <h1>My React Page</h1>
-      <h2 className="bio">This is my first React page, welcome!</h2>
+      <h1 style={ h1Style }>My React Page</h1>
+      <h2 className="bio">This is my first React page, hello!</h2>
       </div>
   );
 }
+
 
 export default App;
