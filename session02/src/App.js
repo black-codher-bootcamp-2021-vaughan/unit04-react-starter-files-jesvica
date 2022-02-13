4475684aca6434bfa533c10dff866018213a2514
jesvica <jvictoriaw@hotmail.co.uk>
2022-02-13 15:36:36 +0000

    added files

---
+++ session02/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React Page</h1>
+      <h2 className="bio">This is my first React page, welcome!</h2>
+      </div>
+  );
+}
+
+export default App;
