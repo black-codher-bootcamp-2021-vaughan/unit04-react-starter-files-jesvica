14cb8cb978ea57ca550c1758ec44557a792ab716
jesvica <jvictoriaw@hotmail.co.uk>
2022-02-20 03:45:43 +0000

    task 1 - profile function

--- session03/task01/src/index.js
+++ session03/task01/src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = '[YOUR NAME GOES HERE]';
 
-function Profile(name){
-  //  if(){
+const name = "Jessica";
+
+function Profile() {
+   if (name) {
     return <main id="content" role="main" class="base">
             <h1>{name}'s React Page</h1>
-              <p>Example of react JSX in action</p>
+              <p>I love to travel!</p>
             </main>
-  //  }
-  //  else {
-  //    return <main id="content" role="main" class="base">
-  //      <h1>Just a React Page</h1>
-  //      <p>Nothing to see here!</p>
-  //    </main>  
-  //  }
-}
+   }
+   else {
+     return <main id="content" role="main" class="base">
+       <h1>Just a React Page</h1>
+       <p>Nothing to see here!</p>
+     </main>  
+   }
+};
+
+Profile(name);
 
 ReactDOM.render(Profile(),document.getElementById('root'));
