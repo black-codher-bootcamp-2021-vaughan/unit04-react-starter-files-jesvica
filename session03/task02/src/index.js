94bf2ee034aa0f33832aae83868445d5fb74baef
jesvica <jvictoriaw@hotmail.co.uk>
2022-02-20 13:32:45 +0000

    task 2 - functional components properties

--- session03/task02/src/index.js
+++ session03/task02/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import Profile from './Profile';
 
 function App() {
+  if(Profile) {
   return (
-      <Profile name="[YOUR NAME]" bio="[YOUR SHORT BIO GOES HERE]" />
+      <Profile name="Jessica" bio="I love coding and design!" />
   );
-}
+} else {
+  return "#content";
+}};
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
